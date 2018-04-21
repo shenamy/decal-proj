4cbb3375de55e861bb36f47f0eb4d070c5a73b04
Sharie Wang <sharie@berkeley.edu>
2018-04-21 03:07:10 -0700

    idk how 2 seed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,10 @@ class HomeController < ApplicationController
   def index
     @user = User.find(current_user.id)
   end
+
+  def store
+    #@user = User.find(current_user.id)
+    @sheens = Avatar.all 
+
+  end
 end
